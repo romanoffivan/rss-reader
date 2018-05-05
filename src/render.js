3418153276f082cf5e5a5ba1e7160bbd306e8e1c
romanoffivan <iw.romanof@yandex.ru>
2018-05-05 15:05:58 +0300

    update render.js

--- src/render.js
+++ src/render.js
@@ -1,5 +1,7 @@
-export default (rssList, data) => {
-  rssList.prepend(`<div class="col-6">
+import $ from 'jquery';
+
+export const getRenderedList = (rssList, data) => {
+  rssList.prepend(`<div id="${data.flowId}" class="col-6">
       <h2 class='title'>${data.title}</h2>
       <p class='description'>${data.description}</p>
       <ul class="list-unstyled list-group">
@@ -13,4 +15,15 @@ export default (rssList, data) => {
       </li>`).join('')}
       </ul>
     </div>`);
+};
+
+export const getUpdatedList = (id, arr) => {
+  $(`#${id}`).find('ul').prepend(`${arr.map(({ articleTitle, link, articleDesc }) => `
+      <li class="list-group-item mb-2">
+        <a href=${link}>${articleTitle}</a>
+        </br>
+        <button type="button" class="btn btn-primary btn-sm btn-outline-dark" data-whatever="${articleDesc}" data-toggle="modal" data-target="#descriptionModal">
+          Описание
+        </button>
+      </li>`).join('')}`);
 };
